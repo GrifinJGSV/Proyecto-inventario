ccbbbd09bfb1fe7bf64d8ac4a5c073aa2addeeef
Josue <jvalladaressevilla@gmail.com>
2023-06-17 19:54:54 -0600

    spring 1

---
+++ src/Principal/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+import Vistas.Proveedores;
+
+/**
+ *
+ * @author Josue
+ */
+public class Principal {
+    
+    public static void main (String [] args){
+        Proveedores proveedores = new Proveedores();
+        proveedores.setVisible(true);
+    }
+    
+}
