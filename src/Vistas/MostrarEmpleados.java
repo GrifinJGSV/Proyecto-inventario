b7c13bd21b5184042415cd8400dcd86bf0f55392
Josue <jvalladaressevilla@gmail.com>
2023-07-11 02:38:29 -0600

    Josue
    botones

--- src/Vistas/MostrarEmpleados.java
+++ src/Vistas/MostrarEmpleados.java
@@ -129,7 +129,9 @@ public class MostrarEmpleados extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        EmpleadosStore empleados = new EmpleadosStore();
+        empleados.setVisible(true);
+    
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
